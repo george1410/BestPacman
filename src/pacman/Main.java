e0de8a98a28c11a478de5fcc3abe34d6943aff64
George McCarron <hello@georgemccarron.com>
2018-11-29 17:36:10 +0000

    🗑 removed some unused imports.

--- src/pacman/Main.java
+++ src/pacman/Main.java
@@ -4,11 +4,8 @@ package pacman;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 public class Main extends Application {
