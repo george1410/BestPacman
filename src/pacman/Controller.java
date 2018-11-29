614d60805952802775dc1bc7e07b3b208231b07b
George McCarron <hello@georgemccarron.com>
2018-11-29 16:04:32 +0000

    🏁 got basic start and menu screens set up.

--- src/pacman/Controller.java
+++ src/pacman/Controller.java
@@ -1,6 +1,28 @@
 package pacman;
 
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import java.io.IOException;
 
 public class Controller {
+    public Button showSetupBtn;
+
+    @FXML
+    void startGame() {
+        System.out.println("Starting Game...");
+    }
+
+    @FXML
+    void showSetup() throws IOException {
+        System.out.println("Setup Menu...");
+        Parent p = FXMLLoader.load(getClass().getResource("setup.fxml"));
+        Scene theScene = new Scene( p );
+        Stage stage = (Stage) showSetupBtn.getScene().getWindow();
+        stage.setScene( theScene );
+    }
 }
--- src/pacman/Main.java
+++ src/pacman/Main.java
@@ -3,7 +3,9 @@ package pacman;
 
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.input.KeyEvent;
@@ -13,13 +15,14 @@ public class Main extends Application {
 
     @Override
     public void start(Stage theStage) throws Exception{
-//        Parent root = FXMLLoader.load(getClass().getResource("pacman.fxml"));
+        Parent p = FXMLLoader.load(getClass().getResource("start.fxml"));
         theStage.setTitle( "Pacman" );
 
         Group root = new Group();
-        Scene theScene = new Scene( root );
+        Scene theScene = new Scene( p );
         theStage.setScene( theScene );
 
+        /*
         Canvas canvas = new Canvas( 1225, 600 );
         root.getChildren().add( canvas );
         GameManager gameManager = new GameManager(root);
@@ -29,7 +32,7 @@ public class Main extends Application {
         theScene.addEventHandler(KeyEvent.KEY_PRESSED, event -> gameManager.getPacman().move(event));
         theScene.addEventHandler(KeyEvent.KEY_RELEASED, event -> gameManager.getPacman().stop(event));
         theScene.addEventHandler(KeyEvent.KEY_PRESSED, event -> gameManager.restartGame(event));
-
+        */
         theStage.show();
     }
 
