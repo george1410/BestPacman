983c34508050d7c8092fc44a5f3f6ed983e164a4
George McCarron <hello@georgemccarron.com>
2018-12-07 13:44:25 +0000

    🏁🏆 current score displays at the end of each round.

--- src/pacman/controllers/NextroundController.java
+++ src/pacman/controllers/NextroundController.java
@@ -1,14 +1,19 @@
 package pacman.controllers;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.text.Text;
+import pacman.GameManager;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class NextroundController implements Initializable {
+    @FXML
+    Text currentScore;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        currentScore.setText("CURRENT SCORE: " + GameManager.getInstance().getScore());
     }
 }
